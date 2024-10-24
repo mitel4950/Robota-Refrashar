99de1ae1889f934197f7dd613d5d7d15b071fd24
Olex Mashchenko <mitel4950@gmail.com>
2024-10-24 22:18:37 +0300

    Initial commit

---
+++ src/main/java/pet/marshmallow/robota_refresher/RobotaRefresherApplication.java
@@ -0,0 +1,13 @@
+package pet.marshmallow.robota_refresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotaRefresherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RobotaRefresherApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pet/marshmallow/robota_refresher/RobotaRefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.marshmallow.robota_refresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotaRefresherApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
